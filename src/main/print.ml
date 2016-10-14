5d4fc31f604ee0bf74d28797573af27e45cc7010
Chris Lane <christopher_lane@live.co.uk>
2016-10-14 23:28:17 +0100

    Improve error messages

--- src/main/print.ml
+++ src/main/print.ml
@@ -1,16 +1,22 @@
 open Lexer
 open Printf
+open Lexing
+exception Error
 
 let parse_with_error lexbuf =
   try Parser.program Lexer.read lexbuf with
-  | SyntaxError msg ->  print_endline msg; exit (-1)
-  | Parser.Error    ->  print_endline ("Found: \'" ^ Lexing.lexeme lexbuf ^ "\' but expected something else.\n"); exit (-1)
+  | SyntaxError msg ->  let _ = Printf.eprintf "%s%!" msg in raise Error
+  | Parser.Error    ->  
+    let pos = Lexing.lexeme_start_p lexbuf in
+    let col = pos.pos_cnum - pos.pos_bol in
+    let _ = Printf.eprintf "Syntax error at line %d and column %d: %s.\n%!"
+        pos.pos_lnum col (Lexing.lexeme lexbuf) in
+    raise Error;;
 
 let parse_file filename = open_in filename
-  |> Lexing.from_channel
-  |> parse_with_error
-  |> List.map Ast.function_string
-  |> String.concat " "
-  |> print_string
-  |> print_newline
-
+                          |> Lexing.from_channel
+                          |> parse_with_error
+                          |> List.map Ast.function_string
+                          |> String.concat " "
+                          |> print_string
+                          |> print_newline;;
