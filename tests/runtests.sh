6c935feebb719f704ea91e21dd624c203781636c
Chris Lane <christopher_lane@live.co.uk>
2016-10-15 01:15:22 +0100

    Remove extra options in script

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -7,7 +7,7 @@ do
     output=$(basename "$file" | cut -d. -f1)
     origin=$(dirname "$file")
     
-    if $(diff -w <(../main "$file") <(cat $origin/output/$output.out) 1>&2); then
+    if $(diff <(../main "$file") <(cat $origin/output/$output.out)); then
         echo "Passed test for file: $file"
     else
         echo "Failed test for file: $file"
