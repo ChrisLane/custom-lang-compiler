9f2af96156f14e33e404c49f72b72fa66ee14260
Chris Lane <christopher_lane@live.co.uk>
2016-10-15 02:26:05 +0100

    Fix test script failing on bad parse tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+set -e
 
 dir=$1
+
+cd $(dirname $0)
 
 for file in **/*.src
 do
     output=$(basename "$file" | cut -d. -f1)
     origin=$(dirname "$file")
-    
-    if $(diff <(../main "$file") <(cat $origin/output/$output.out)); then
+
+    if [ -z "$(diff <(../main "$file" 2>&1 || true) $origin/output/$output.out)" ]; then
         echo "Passed test for file: $file"
     else
         echo "Failed test for file: $file"
