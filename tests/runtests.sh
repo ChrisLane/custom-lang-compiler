52cba29669d186f2ab3e206677d4aa3c53089c50
Chris Lane <christopher_lane@live.co.uk>
2016-10-18 23:29:34 +0100

    Adjust test script to work for new tasks

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -10,7 +10,7 @@ do
     output=$(basename "$file" | cut -d. -f1)
     origin=$(dirname "$file")
 
-    if [ -z "$(diff <(../main "$file" 2>&1 || true) $origin/output/$output.out)" ]; then
+    if [ -z "$(diff <(../main parse "$file" 2>&1 || true) $origin/output/$output.out)" ]; then
         echo "Passed test for file: $file"
     else
         echo "Failed test for file: $file"
