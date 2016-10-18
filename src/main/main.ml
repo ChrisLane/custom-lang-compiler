2fcb9cbc987b335ee4866e5325cddb1ad8f49603
Chris Lane <christopher_lane@live.co.uk>
2016-10-18 23:27:06 +0100

    Add options for running different tasks

--- src/main/main.ml
+++ src/main/main.ml
@@ -1,9 +1,19 @@
 (* Print command usage information *)
-let help () = print_endline ("Please use: './main <file>\n")
+let print_help () =
+  print_endline ("\nPlease use: './main <task> <file>'\n");
+  print_endline ("Available tasks:");
+  print_endline ("\tparse - Parse a source file");
+  print_endline ("\tevaluate - Evaluate a source file");
+  print_endline "";;
 
-(* If more/less than 1 arguement is given, print proper command usage *)
+(* Run the appropriate command *)
+let run_task task file = match task with
+  | "parse" -> Print.parse_file file
+  | "evaluate" -> Print.eval_file file
+  | _ -> print_endline "\nInvalid task."; print_help(); exit 1
+
+(* If more/less than 2 arguement is given, print proper command usage *)
 let _ =
-  if Array.length (Sys.argv) != 2
-  then (help (); exit 1)
-  else Print.parse_file (Sys.argv.(1))
-
+  if (Array.length Sys.argv) = 3
+  then run_task Sys.argv.(1) Sys.argv.(2)
+  else print_help(); exit 1
