5b5cd486f5472863722f0094481081d0feda83b2
Chris Lane <christopher_lane@live.co.uk>
2016-10-20 18:52:41 +0100

    Add evaluation tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,6 +3,12 @@ set -e
 
 cd $(dirname $0)
 
+fails=0
+
+echo
+echo "Parsing Tests"
+echo "============="
+
 for file in parse/**/*.src
 do
     output=$(basename "$file" | cut -d. -f1)
@@ -11,6 +17,25 @@ do
     if [ -z "$(diff <(../main parse "$file" 2>&1 || true) $origin/output/$output.out)" ]; then
         echo "Passed test for file: $file"
     else
-        echo "Failed test for file: $file" && exit 1
+        echo "Failed test for file: $file" && fails=1
     fi
 done
+
+echo
+echo "Evaluation Tests"
+echo "================"
+
+for file in evaluate/**/*.src
+do
+    output=$(basename "$file" | cut -d. -f1)
+    origin=$(dirname "$file")
+
+    if [ -z "$(diff <(../main evaluate "$file" 2>&1 || true) $origin/output/$output.out)" ]; then
+        echo "Passed test for file: $file"
+    else
+        echo "Failed test for file: $file" && fails=1
+    fi
+done
+
+echo
+exit $fails
