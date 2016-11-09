7160f09b00062329c7a4067a8806f840ef810124
Chris Lane <christopher_lane@live.co.uk>
2016-11-09 19:44:25 +0000

    Cleanup non-changing matches

--- src/main/optim.ml
+++ src/main/optim.ml
@@ -52,7 +52,7 @@ let rec lookup x = function
 (* Find a variable in variable storage and update it's value *)
 let rec update x v = function
   | []                          -> failwith "Could not find a variable to update."
-  | (y, Ref z)::ys  when x = y  -> Hashtbl.replace store (string_of_int z) v
+  | (y, Ref z)::ys  when x = y  -> replace store (string_of_int z) v
   | y::ys                       -> update x v ys
 
 (* References for variable pointers *)
@@ -61,12 +61,6 @@ let newref() = addr_gbl:=!addr_gbl+1; !addr_gbl
 
 (* Optimise an expression *)
 let rec optim_exp env = function
-  | Empty                   -> Empty
-  | Const e                 -> Const e
-  | Bool e                  -> Bool e
-  | Ref e                   -> Ref e
-  | Unknown                 -> Unknown
-
   | Identifier e            -> lookup e env
 
   | Deref e                 -> (match optim_exp env e with
@@ -84,18 +78,21 @@ let rec optim_exp env = function
       | n             -> If (n, optim_exp env f, optim_exp env g))
 
   | Operator (op, e, f)     -> optim_operator op (optim_exp env e) (optim_exp env f)
+
   | Asg (Identifier e, f)   -> let v = optim_exp env f in (match v with
       | Readint -> update e Unknown env; Asg(Identifier e, v)
       | _       -> update e v env; Asg (Identifier e, v))
+
   | Seq (e, Empty)          -> optim_exp env e
+
   | Seq (e, f)              ->
     let v = optim_exp env e in
     let v2 = optim_exp env f in (match v with
         | Empty -> v2
         | _     -> Seq (v, v2))
+
   | Print e                 -> Print (optim_exp env e)
-  | Application (e, f)      -> Application (e, f)
-  | Readint                 -> Readint
+
   | Let (x, e, f)           -> (match optim_exp env e with
       | Const n -> optim_exp ((x, Const n)::env) f
       | Bool n  -> optim_exp ((x, Bool n)::env) f
@@ -131,11 +128,10 @@ let rec optim_exp env = function
     remove store (string_of_int l);
     New (x, optim_exp env e, v2)
 
-
   | e                       -> e
 
 (* Optimise a function *)
 let rec optim_program = function
   | [] -> []
   | Fundef ("main", args, body)::ys -> Fundef ("main", args, (optim_exp [] body))::optim_program ys
-  | f                               -> f
+  | f -> f
